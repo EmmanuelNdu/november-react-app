05343e3ee5eaaf33c88cbd918b846f5ac5fe7f80
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-08 11:40:38 +0100

    Fixed some bugs in the Navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
+import logo from "../../assets/logo.svg";
 
+// BEM Block ELement Modifier
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="gtp3__navbar"> 
+      <div className='gtp3__navbar-links'>
+        <div className='gtp3__navbar-links_logo'>
+          <img src={logo} alt='logo' />
+        </div>
+      </div>
+    </div>
   )
 }
 
