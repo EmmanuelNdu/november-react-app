d5817073bcb9f8fce9b75505f318c839cfb8209a
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-09 00:28:03 +0100

    updated the styles in the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import logo from "../../assets/logo.svg";
 
+const Menu = () => (
+  <>
+          <p><a href='#Home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT3</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
+  </>
+)
+
 // BEM Block ELement Modifier
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
-    <div className="gtp3__navbar"> 
-      <div className='gtp3__navbar-links'>
-        <div className='gtp3__navbar-links_logo'>
+    <div className="gpt3__navbar"> 
+      <div className='gpt3__navbar-links'>
+        <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt='logo' />
         </div>
+        <div className='gpt3__navbar-links_container'>
+          <Menu />
+        </div>
+      </div>
+      <div className='gpt3__navbar-sign'>
+        <p>Sign In</p>
+        <button type='button'>Sign Up</button>
+      </div>
+      <div className='gpt3__navbar-menu'>
+        {toggleMenu 
+        ? <RiCloseLine color = "#fff" size={27} onClick={ () => setToggleMenu(false)} />
+        :  <RiMenu3Line color = "#fff" size={27} onClick={ () => setToggleMenu(false)} />
+        }
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <Menu />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
