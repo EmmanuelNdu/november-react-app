df54eab12174a9b236f8962cfbb87efd0832ff72
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-10 00:00:10 +0100

    updated the navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -22,10 +22,10 @@ const Navbar = () => {
         <div className='gpt3__navbar-links_logo'>
           <img src={logo} alt='logo' />
         </div>
-        <div className='gpt3__navbar-links_container'>
+      </div>
+      <div className='gpt3__navbar-links_container'>
           <Menu />
         </div>
-      </div>
       <div className='gpt3__navbar-sign'>
         <p>Sign In</p>
         <button type='button'>Sign Up</button>
