62005d10a83295d16ea5701cc2236bbbf6de2d07
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-10 01:39:54 +0100

    Completed the whatgpt3 component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './whatgpt3.css';
+import { Feature } from '../../components';
 
 const WhatGPT3 = () => {
   return (
-    <div>WhatGPT3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature />
+      </div>
+      <div className='gpt3__whatgpt3-heading'>
+        <h1 className='gradient__text'>The Possibilities are beyond your imagnation</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className='gpt3__whatgpt3-containner'>
+        <Feature />
+        <Feature />
+        <Feature />
+      </div>
+    </div>
   )
 }
 
