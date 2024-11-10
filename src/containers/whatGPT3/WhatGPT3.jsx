ee49ffd7c30d27336a39290f22fb11f15abb3898
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-10 23:27:00 +0100

    completed the Feature component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
-import './whatgpt3.css';
+import './whatGPT3.css';
 import { Feature } from '../../components';
 
 const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='whpt3'>
       <div className='gpt3__whatgpt3-feature'>
-        <Feature />
+        <Feature title = "What is GPT-3" text = "The mountain trail was steep, yet the vibrant leaves painted in autumn,yet the vibrant leaves painted in autumn." />
       </div>
       <div className='gpt3__whatgpt3-heading'>
         <h1 className='gradient__text'>The Possibilities are beyond your imagnation</h1>
         <p>Explore The Library</p>
       </div>
-      <div className='gpt3__whatgpt3-containner'>
-        <Feature />
-        <Feature />
-        <Feature />
+      <div className='gpt3__whatgpt3-container'>
+        <Feature title = "Chatbots" text = "The mountain trail was steep, yet the vibrant leaves painted in autumn,yet the vibrant leaves painted in autumn." />
+        <Feature title = "Knowledgebase" text = "The mountain trail was steep, yet the vibrant leaves painted in autumn,yet the vibrant leaves painted in autumn." />
+        <Feature title = "Education" text = "The mountain trail was steep, yet the vibrant leaves painted in autumn,yet the vibrant leaves painted in autumn." />
       </div>
     </div>
   )
