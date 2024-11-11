1a34d22b5e64df94dac5c2dbaece86536aea5dd4
Ndubuaku Emmanuel <ndubuaku.emmanuel@gmail.com>
2024-11-11 02:08:07 +0100

    Started the Possibility component

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './possibility.css';
+import possibilityImage from '../../assets/possibility.png';
 
 const Possibility = () => {
   return (
-    <div>Possibility</div>
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className='gpt3__possibility-image'>
+        <img src= { possibilityImage } alt='possibility'/>
+      </div>
+      <div className='gpt3_possibility-content'>
+        <h4>Request Early Access to get Started</h4>
+        <h1 className='gradient-text'>The Possibility are Beyond Your Imagination</h1>
+        <p>Predictable CSS: With a structured naming system, you can easily figure out which class applies.</p>
+        <h4>Request Early Access to get Started</h4>
+      </div>
+    </div>
   )
 }
 
